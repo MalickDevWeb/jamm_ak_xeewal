949593f371701c55b577c56e265ce4299c1e500a
MalickDevWeb <malickteuw.devweb@gmail.com>
2026-08-28 19:48:21 +0000

    premier commit

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://jamm-backend.vercel.app/api/v1'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://jamm-api.onrender.com/api/v1' // Update with real production URL later
+  production: false,
+  apiUrl: 'http://localhost:3001/api/v1'
 };
