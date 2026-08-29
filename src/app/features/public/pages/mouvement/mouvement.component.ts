8f50dcc678085bae74ecb4bb927f0d69d787d1c8
MalickDevWeb <malickteuw.devweb@gmail.com>
2026-08-29 21:28:16 +0000

    Refonte page Mouvement: design moderne, hero, histoire, vision/mission/valeurs, chiffres

--- src/app/features/public/pages/mouvement/mouvement.component.ts
+++ src/app/features/public/pages/mouvement/mouvement.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 import { PublicDataService } from '../../../../core/services/public-data.service';
 
 @Component({
   selector: 'app-mouvement',
   standalone: true,
   imports: [CommonModule, RouterLink],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './mouvement.component.html',
   styleUrl: './mouvement.component.css'
 })
-export class MouvementComponent implements OnInit {
+export class MouvementComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<void>();
   content: any = null;
 
-  constructor(private publicData: PublicDataService) {}
+  constructor(
+    private publicData: PublicDataService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
-    this.publicData.getEditorial('mouvement').subscribe({
+    this.publicData.getEditorial('mouvement').pipe(
+      takeUntil(this.destroy$)
+    ).subscribe({
       next: (res: any) => {
         if (res.data) {
           this.content = res.data;
+          this.cdr.markForCheck();
         }
       }
     });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
