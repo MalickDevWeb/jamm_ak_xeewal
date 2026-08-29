e4a2170ee4b9aafaca6ba36df5ae540de07a71df
MalickDevWeb <malickteuw.devweb@gmail.com>
2026-08-29 17:28:55 +0000

    ggfll

--- src/app/features/engagement/pages/adherer/adherer.component.ts
+++ src/app/features/engagement/pages/adherer/adherer.component.ts
@@ -2,9 +2,8 @@ import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRe
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, takeUntil, firstValueFrom } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs/operators';
 import { environment } from '../../../../../environments/environment';
 import { PublicDataService, Option } from '../../../../core/services/public-data.service';
 
